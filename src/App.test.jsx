f259988c29d779eeeb1aa3b48f82b3b373cfdc34
Toan Nguyen <tnguyen@3537gmail.com>
2024-10-16 06:02:11 -0500

    first commit

---
+++ src/App.test.jsx
@@ -0,0 +1,31 @@
+import App from "./App";
+import { test } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import TodoTable from "./TodoTable";
+
+// test("renders App component", () => {
+//   // renders the App component for testing
+//   render(<App />);
+//   const header = screen.getByText(/My Todolist/i);
+//   expect(header).toBeInTheDocument();
+// });
+
+test("renders todotable", () => {
+  const row = [{ desc: "Go to coffee", date: "24.01.2023" }];
+  render(<TodoTable todos={row} />);
+  const table = screen.getByRole("table");
+  expect(table).toHaveTextContent(/go to coffee/i);
+});
+
+test("add todo", () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText("Description");
+  fireEvent.change(desc, { target: { value: "Go to coffee" } });
+  const date = screen.getByPlaceholderText("Date");
+  fireEvent.change(date, { target: { value: "29.01.2023" } });
+  const button = screen.getByText("Add");
+  fireEvent.click(button);
+  const table = screen.getByRole("table");
+  expect(table).toHaveTextContent(/go to coffee/i);
+});
